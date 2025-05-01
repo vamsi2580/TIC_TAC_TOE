4f1bbfbee25e721272fd179965c4a1481f8cc6a0
vamsi2580 <vamsimanoj09@gmail.com>
2025-05-01 18:10:28 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,57 @@
+let turn = false;
+const winPatterns = [
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+];
+const buttons=document.querySelectorAll(".Box button");
+const rstbtn=document.querySelector(".reset");
+const congra=document.querySelector(".hide");
+function reset(){
+    buttons.forEach(button =>{
+        button.innerText="";
+        button.disabled=false;
+        turn=false;
+        congra.innerText="";
+    });
+}
+function btclick(event){
+    if(turn){
+        event.target.innerText="O";
+        event.target.disabled=true;
+        turn=false;
+    }
+    else{
+        event.target.innerText="X";
+        event.target.disabled=true;
+        turn=true;
+    }
+    checkWinner();
+}
+
+function call(btn){
+    btn.addEventListener("click",btclick);
+}
+
+buttons.forEach(call);
+rstbtn.addEventListener("click",reset);
+
+function checkWinner() {
+    for(let patterns of winPatterns){
+        let val1=buttons[patterns[0]].innerText;
+        let val2=buttons[patterns[1]].innerText;
+        let val3=buttons[patterns[2]].innerText;
+    
+    if (val1!="" && val2!="" && val3!=""){
+        if (val1==val2 && val2==val3){
+            console.log("winner");
+            congra.innerText=`Congratulations ${val1}`;
+            }
+        }
+    }
+}
